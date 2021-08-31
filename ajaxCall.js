d8ec17a1f9fac314383c07cb879ad4a5fc77a51b
Andrew Y <andrew@compandsave.com>
2021-08-31 10:10:50 -0700

    support formData

--- ajaxCall.js
+++ ajaxCall.js
@@ -1,4 +1,14 @@
-function ajaxCall(type, url, data, resolve, reject) {
+/**
+ * Api Call by AJAX
+ * 
+ * @param {string} type Api call type, e.g, GET, POST
+ * @param {string} url Api url endpoint
+ * @param {object} [data] Data object if formData is not provided
+ * @param {function} resolve Resolve function
+ * @param {function} reject Reject function
+ * @param {formData} [formData] If provided, formData will be used
+ */
+ export const ajaxCall = (type, url, data, resolve, reject, formData)=> {
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     try {
@@ -7,9 +17,8 @@ function ajaxCall(type, url, data, resolve, reject) {
     }
     catch (e) { reject(e); }
   };
-  xhttp.open(type, url, true);
-  xhttp.setRequestHeader("Content-Type", "application/json");
-  xhttp.send(JSON.stringify({ data: data }));
-}
 
-export { ajaxCall };
+  xhttp.open(type, url, true);
+  if (typeof formData === "undefined") { xhttp.setRequestHeader("Content-Type", "application/json"); }
+  xhttp.send(typeof formData === "undefined" ? JSON.stringify({ data: data }) : formData);
+}
